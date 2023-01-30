c52cef46faaad9cff9e0c6303a373abe0ff0c4e3
Anderson Brito <andersonfbrito@gmail.com>
2023-01-30 17:08:57 -0300

    new multibar

--- figures/barplot/pandas_multibar.py
+++ figures/barplot/pandas_multibar.py
@@ -8,13 +8,14 @@ import matplotlib.ticker as mtick
 import numpy as np
 import os
 import argparse
+import matplotlib.lines as mlines
 
-#plt.rcParams['font.family'] = 'Open Sans'
-plt.rcParams['font.family'] = ['Open Sans Light', 'sans-serif'] #linux option
+plt.rcParams['font.family'] = 'Arial'
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description="Generate line plots",
+        description="Generate bar plots",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument("--config", required=True, help="Configuration file in TSV format")
@@ -22,10 +23,9 @@ if __name__ == '__main__':
 
     config = args.config
 
-    # path = '/Users/anderson/google_drive/ITpS/projetos_itps/sgtf_omicron/analyses/run15_20220621_sgtf/figures/barplot/'
+    # path = '/Users/Anderson/Library/CloudStorage/GoogleDrive-anderson.brito@itps.org.br/Outros computadores/My Mac mini/google_drive/ITpS/projetos_itps/vigilanciagenomica/analyses/relatorio10_20230105/figures/barplot/brazil/raw_data/'
     # os.chdir(path)
-    # config = 'config_bardetection.tsv'
-
+    # config = 'config_variants_weeks.tsv'
 
     def load_table(file):
         df = ''
@@ -78,7 +78,6 @@ if __name__ == '__main__':
                 new_df = new_df.append(df_filtered)
             else:
                 new_df = new_df[new_df[filter_col].isin(filter_val)]
-            # print(new_df)#.head())
 
         exclude = {}
         for filter_value in criteria.split(','):
@@ -119,12 +118,16 @@ if __name__ == '__main__':
     if ignore_cols not in ['', None]:
         for col in ignore_cols.split(','):
             dfD = dfD.drop(columns=col.strip())
-    # print(dfD.columns.tolist())
 
 
     # define datacolumns
     y_var = params.loc['y_var', 'value']
     group = params.loc['groupby', 'value']
+    if group in ['', None]:
+        group = 'mock_column'
+        dfD[group] = ''
+
+
     datacols = sorted(list(set(dfD.columns.tolist()) - set([y_var, group])))
     dfD[datacols] = dfD[datacols].apply(pd.to_numeric, errors='coerce')
 
@@ -143,11 +146,14 @@ if __name__ == '__main__':
     if '.' in colour_scheme:
         colour_by = params.loc['colour_by', 'value']
         dfA = load_table(colour_scheme)
+        dfA = dfA[dfA['field'].isin([colour_by])]
         dfA = dfA.rename(columns={'value': colour_by})
         dfA = dfA.set_index(colour_by)
         for c in dfD[colour_by].tolist():
             if c not in colourmap and c in dfD[y_var].tolist():
                 colourmap[c] = dfA.loc[c, 'hex_color']
+                # print(dfA.loc[c, 'hex_color'])
+
 
     # plot specifications
     plot_width = float(params.loc['figsize', 'value'].split(',')[0].strip())
@@ -168,23 +174,56 @@ if __name__ == '__main__':
 
     nrows = int(params.loc['nrows', 'value'])
     ncols = int(params.loc['ncols', 'value'])
+
+    legend_pos = params.loc['legend_position', 'value']
+    column_legend = params.loc['column_legend', 'value']
+
+    # if legend_pos not in ['', None]:
+    #     if legend_pos in ['left', 'right']:
+    #         ncols += 1
+    #     else:
+    #         nrows += 1
+
     if nrows == 1 and ncols == 1:
         fig, ax = plt.subplots(figsize=figsize)
     else:
         fig, axes = plt.subplots(nrows, ncols, sharex=boolx, sharey=booly, figsize=figsize)
 
-
     # # sort values
-    # sortby = params.loc['sort_by', 'value']
+    # # sortby = params.loc['sort_by', 'value']
+    # sortby = ''
     # if sortby not in ['', None]:
     #     df1 = df1.sort_values(by=[group, sortby])
     # else:
     #     df1 = df1.sort_values(by=[group, y_var])
-    #
+
     # print(df1[sortby].tolist())
 
-    # dfD = dfD.sort_values(by=[group, y_var])
+    # accomodate legend in subplot, skipping axis that are multiple of ncols in cases where legend_position = vertical
+    used_axes = []
+    jump = 0
+    c = 1
     for i, (name, df) in enumerate(dfD.groupby(group)):
+        oldi = i
+        if legend_pos == 'left':
+            if i >= ncols-1:
+                # print(c % ncols)
+                # c += 1
+                if c % ncols == 0:
+                    jump += 1
+                    i = i + jump
+                    c = 1
+                    # print('jump')
+                else:
+                    i = i + jump
+        c += 1
+
+        # print(oldi, i)
+        cur_ax = (i // ncols, i % ncols)
+        # print(name, cur_ax)
+        used_axes.append(cur_ax)
+        # print('')
+
         # absolute or relative scale?
         scale = params.loc['scale', 'value']
         if scale == 'relative':
@@ -214,29 +253,33 @@ if __name__ == '__main__':
         plot_kind = params.loc['plot_kind', 'value']
         if '.' in colour_scheme:
             ax = df2.plot(ax=ax, kind=plot_kind, stacked=True, color=[colourmap[i] for i in df2.columns], edgecolor='white', lw=0.1,
-            figsize=(plot_width, plot_heigth), width=0.90, zorder=10)
+                          figsize=(plot_width, plot_heigth), width=0.90, zorder=10)
         else:
             if colour_scheme in ['', None]:
                 colour_scheme = 'Greys'
             ax = df2.plot(ax=ax, kind='bar', stacked=True, cmap=colour_scheme, figsize=(plot_width, plot_heigth), width=0.9, zorder=10)
 
 
-        handles, labels = ax.get_legend_handles_labels()
-        legend = params.loc['legend', 'value']
-        if legend not in ['', None]:
-            column_legend = int(params.loc['column_legend', 'value'])
-            ax.legend(handles, labels, loc=legend, ncol=column_legend, fontsize=8)  # reverse both handles and labels
-        else:
-            ax.get_legend().remove()
+        # handles, labels = ax.get_legend_handles_labels()
+        # legend = params.loc['legend', 'value']
+        # if legend not in ['', None]:
+        #     column_legend = int(params.loc['column_legend', 'value'])
+        #     ax.legend(handles, labels, loc=legend, ncol=column_legend, fontsize=8)  # reverse both handles and labels
+        # else:
+        #     ax.get_legend().remove()
 
-
-
+        ax.get_legend().remove()
 
         # labels
         x_label = params.loc['x_label', 'value'].replace('\\n', '\n')
         y_label = params.loc['y_label', 'value'].replace('\\n', '\n')
-        ax.set_xlabel(x_label)
-        ax.set_ylabel(y_label)
+        ax.set_xlabel(x_label, fontsize=8)
+        ax.set_ylabel(y_label, fontsize=8)
+
+        # plot title
+        plot_label = params.loc['plot_label', 'value']
+        if plot_label not in ['', None]:
+            ax.set_title(name, fontsize=10)
 
         # if scale == 'absolute':
         #     tick_every = int(params.loc['tick_every', 'value'])
@@ -254,13 +297,14 @@ if __name__ == '__main__':
         #         ax.xaxis.set_minor_locator(MultipleLocator())
         #         ax.xaxis.set_major_locator(MultipleLocator(tick_every))
         # if plot_kind == 'barh':
+
         if tick_every not in ['', None]:
             ax.xaxis.set_major_locator(MultipleLocator(tick_every))
-            ax.xaxis.set_minor_locator(MultipleLocator(tick_every/2))
+            ax.xaxis.set_minor_locator(MultipleLocator(tick_every/tick_every))
 
 
-    # plt.xticks(ticklabels, rotation=90)
-        # ax.set_ylim((10**-1, 10**4))
+        # plt.xticks(ticklabels, rotation=90)
+            # ax.set_ylim((10**-1, 10**4))
 
         min_y = params.loc['min_y', 'value']
         if min_y not in ['', None]:
@@ -285,15 +329,72 @@ if __name__ == '__main__':
             else:
                 ax.set_ylim(ymin=min_y, ymax=max_y)
 
-        # if scale == 'relative':
-        #     ax.set_xlim(-1, 101)
-        #     ax.xaxis.set_major_formatter(mtick.PercentFormatter())
+        if scale == 'relative':
+            ax.set_ylim(0, 100)
+            ax.yaxis.set_major_formatter(mtick.PercentFormatter())
 
         ax.yaxis.set_tick_params(which='minor', bottom=False)
 
     # ax.axhline(y=np.log(1000), color='#ABABAB', linestyle='--', lw=0.5)
+    # handles, labels = ax.get_legend_handles_labels()
 
-    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=None, hspace=None)
+
+    handles = []
+    for label, color in sorted(colourmap.items()):
+        h = mlines.Line2D([], [], color=color, marker='s', linestyle='None', markersize=10, label=label)
+        handles.append(h)
+
+
+    # print(used_axes)
+    i = 0
+    # print(len(axes))
+    # print(axes)
+    # print(type(axes[0]))
+    if type(axes[0]) == np.ndarray: # more than 2 columns and rows
+        for axset in axes:
+            # print(len(axes))
+            for ax in axset:
+                # print(ax)
+                cur_ax = (i // ncols, i % ncols)
+                # print(cur_ax)
+
+                if cur_ax not in used_axes:
+                    axes[i // ncols][i % ncols].axis('tight')
+                    axes[i // ncols][i % ncols].axis('off')
+                    # print('done')
+
+                i += 1
+    else: # single column or single row
+        # print(len(axes))
+        for ax in axes:
+            # print(ax)
+            cur_ax = (i // ncols, i % ncols)
+
+            # print(cur_ax)
+            # print(used_axes)
+            if cur_ax not in used_axes:
+                axes[i].axis('tight')
+                axes[i].axis('off')
+            i += 1
+
+    if legend_pos not in ['', None]:
+
+        if column_legend not in ['', None]:
+            column_legend = int(column_legend)
+        else:
+            column_legend = 1
+
+        if legend_pos == 'vertical':
+            ax_leg = plt.subplot2grid((nrows, ncols), (0, ncols-1), rowspan=nrows)
+            ax_leg.axis('tight')
+            ax_leg.axis('off')
+            ax_leg.legend(handles=handles, loc='upper left', ncol=column_legend, frameon=False)
+
+        elif legend_pos == 'horizontal':
+            ax_leg = plt.subplot2grid((nrows, ncols), (nrows-1, 0), colspan=ncols)
+            ax_leg.axis('tight')
+            ax_leg.axis('off')
+            ax_leg.legend(handles=handles, loc='upper center', ncol=column_legend, frameon=False)
 
     if backend == 'pdf':
         plt.tight_layout()
@@ -301,3 +402,5 @@ if __name__ == '__main__':
         print("\nDone!\nFile saved as: " + "barplot_" + config.split('.')[0].split('_')[-1] + ".pdf")
     else:
         plt.show()
+
+
