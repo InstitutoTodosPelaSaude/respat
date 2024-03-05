2d8803ae1fc06f906b5cfea1a1b6cd61c6bccf01
João Pedro <joaopedrodasilvalima@gmail.com>
2024-03-05 18:21:51 -0300

    FEAT: Historical combined fix types

---
+++ dbt/models/combined/comb_historical_01_fix_types.sql
@@ -0,0 +1,52 @@
+{{ config(materialized='view') }}
+
+WITH source_data AS (
+
+    SELECT
+        'HISTORICAL_'||sample_id AS sample_id,
+        lab_id,
+        test_id,
+        test_kit,
+        --country,
+        --region,
+        TO_DATE("date_testing", 'YYYY-MM-DD') AS date_testing,
+        state_code,
+        epiweek,
+        patient_id,
+        sex,
+        age::INT,
+        --age_group,	
+        "SC2_test_result",
+        "FLUA_test_result",
+        "FLUB_test_result",
+        "VSR_test_result",
+        "COVS_test_result",
+        "ADENO_test_result",
+        "BOCA_test_result",
+        "RINO_test_result",
+        "PARA_test_result",
+        "ENTERO_test_result",
+        "META_test_result",
+        "BAC_test_result",
+        -- Ct_FluB,
+        -- Ct_geneN,
+        -- Ct_FluA,
+        -- Ct_RDRP,
+        -- Ct_geneE,
+        -- Ct_geneS,
+        -- Ct_VSR,
+        -- Ct_ORF1ab,
+        -- geneS_detection,
+        location,
+        state
+        -- ADM2_PT,
+        -- ADM2_PCODE,
+        -- lat,
+        -- long,
+    FROM
+    {{ source("dagster", "combined_historical_raw") }}
+
+)
+SELECT
+    *
+FROM source_data
--- dbt/models/combined/combined_historical_01.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-WITH source_data AS (
-
-    SELECT * FROM
-    {{ source("dagster", "combined_historical_raw") }}
-
-)
