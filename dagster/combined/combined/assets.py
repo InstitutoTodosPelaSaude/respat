3f51bc86a41ffe8864d03a9b47a5fc6134a59a3a
João Pedro <joaopedrodasilvalima@gmail.com>
2024-03-05 17:44:44 -0300

    FEAT: Combined definitions

---
+++ dagster/combined/combined/assets.py
@@ -0,0 +1,22 @@
+from dagster import AssetExecutionContext
+
+from dagster_dbt import (
+    DbtCliResource, 
+    dbt_assets,
+    DagsterDbtTranslator,
+    DagsterDbtTranslatorSettings
+)
+
+from .constants import dbt_manifest_path
+
+dagster_dbt_translator = DagsterDbtTranslator(
+    settings=DagsterDbtTranslatorSettings(enable_asset_checks=True)
+)
+
+@dbt_assets(
+    manifest=dbt_manifest_path,
+    select='combined',
+    dagster_dbt_translator=dagster_dbt_translator
+)
+def respiratorios_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
