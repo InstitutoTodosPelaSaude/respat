53bad80d0ec60d2d026f82ca9e7974370c9b9921
João Pedro <joaopedrodasilvalima@gmail.com>
2024-03-06 17:27:17 -0300

    FEAT: Sabin 01 convert types

--- dbt/models/sabin/sabin_01_convert_types.sql
+++ dbt/models/sabin/sabin_01_convert_types.sql
@@ -1,1 +1,11 @@
-SELECT 1 AS test
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+
+    SELECT * FROM
+    {{ source("dagster", "einstein_raw") }}
+
+)
+SELECT
+    *
+FROM source_data
