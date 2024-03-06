58cf41f32317327ac5bf07f11adabe33f6cedd99
João Pedro <joaopedrodasilvalima@gmail.com>
2024-03-06 16:28:34 -0300

    FEAT: Remove lines without any tests results, fix sex column

--- dbt/models/combined/combined_historical_02_fix_values.sql
+++ dbt/models/combined/combined_historical_02_fix_values.sql
@@ -16,7 +16,11 @@ SELECT
         date_testing,
         state_code,
         patient_id,
-        sex,
+        
+        CASE 
+            WHEN not sex in ('M', 'F') THEN NULL
+            ELSE sex 
+        END AS sex,
         
         CASE 
             WHEN age < 0 THEN NULL
@@ -38,4 +42,20 @@ SELECT
         location,
         state
 FROM source_data
-WHERE date_testing < '2024-01-01'
+WHERE 1=1
+AND date_testing < '2024-01-01' -- Apenas dados históricos
+AND NOT ( 
+    -- Filtrar linhas sem nenhum resultado de teste
+    "SC2_test_result" = 'NT' AND
+    "FLUA_test_result" = 'NT' AND
+    "FLUB_test_result" = 'NT' AND
+    "VSR_test_result" = 'NT' AND
+    "RINO_test_result" = 'NT' AND
+    "META_test_result" = 'NT' AND
+    "PARA_test_result" = 'NT' AND
+    "ADENO_test_result" = 'NT' AND
+    "BOCA_test_result" = 'NT' AND
+    "COVS_test_result" = 'NT' AND
+    "ENTERO_test_result" = 'NT' AND
+    "BAC_test_result" = 'NT'
+)
