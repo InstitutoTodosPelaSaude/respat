a70a5deb210a1e5c0ae6ba0a6d5ca42129ec8022
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-03-22 16:28:55 -0300

    FEAT: Initial HLAGyn structure

---
+++ dagster/lab_hlagyn/lab_hlagyn/assets.py
@@ -0,0 +1,32 @@
+from dagster import (
+    AssetExecutionContext,
+    asset,
+    MaterializeResult, 
+    MetadataValue
+)
+from dagster_dbt import (
+    DbtCliResource, 
+    dbt_assets,
+    DagsterDbtTranslator,
+    DagsterDbtTranslatorSettings
+)
+from textwrap import dedent
+import pandas as pd
+import os
+import pathlib
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+from .constants import dbt_manifest_path
+
+dagster_dbt_translator = DagsterDbtTranslator(
+    settings=DagsterDbtTranslatorSettings(enable_asset_checks=True)
+)
+
+@dbt_assets(
+    manifest=dbt_manifest_path,
+    select='hlagyn',
+    dagster_dbt_translator=dagster_dbt_translator
+)
+def respiratorios_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
