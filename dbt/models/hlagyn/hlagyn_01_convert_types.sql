59f3f9aeb00f3cb77d8e65907e53bff27c81c5a9
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-03-22 20:05:39 -0300

    FEAT: hlagyn dbt initial steps

--- dbt/models/hlagyn/hlagyn_01_convert_types.sql
+++ dbt/models/hlagyn/hlagyn_01_convert_types.sql
@@ -1,1 +1,49 @@
-select 1 as id;
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+    SELECT * FROM
+    {{ source("dagster", "hlagyn_raw") }}
+)
+SELECT
+    "Pedido"                                    AS test_id,
+    TO_DATE("Data Coleta", 'YYYY-MM-DD')        AS date_testing,
+    "Idade"::INT                                AS age,
+    {{ normalize_text("Sexo") }}                AS sex,
+    {{ normalize_text("Métodologia") }}         AS detalhe_exame,
+    {{ normalize_text("Cidade") }}              AS location,
+    {{ normalize_text("UF") }}                  AS state_code,
+
+    {{ normalize_text("Resultado") }}           AS result_covid,
+
+    {{ normalize_text("Vírus Influenza A") }}   AS result_virus_influenza_a,
+    {{ normalize_text("Vírus Influenza B") }}   AS result_virus_influenza_b,
+    {{ normalize_text("Vírus Sincicial Respiratório A/B") }} AS result_virus_sincicial_respiratorio,
+    {{ normalize_text("Coronavírus SARS-CoV-2") }} AS result_virus_sars_cov_2,
+
+    {{ normalize_text("VIRUS_IA") }}            AS result_virus_ia,
+    {{ normalize_text("VIRUS_H1N1") }}          AS result_virus_h1n1,
+    {{ normalize_text("VIRUS_AH3") }}           AS result_virus_ah3,
+    {{ normalize_text("VIRUS_B") }}             AS result_virus_b,
+    {{ normalize_text("VIRUS_MH") }}            AS result_virus_mh,
+    {{ normalize_text("VIRUS_SA") }}            AS result_virus_sa,
+    {{ normalize_text("VIRUS_SB") }}            AS result_virus_sb,
+    {{ normalize_text("VIRUS_RH") }}            AS result_virus_rh,
+    {{ normalize_text("VIRUS_PH") }}            AS result_virus_ph,
+    {{ normalize_text("VIRUS_PH2") }}           AS result_virus_ph2,
+    {{ normalize_text("VIRUS_PH3") }}           AS result_virus_ph3,
+    {{ normalize_text("VIRUS_PH4") }}           AS result_virus_ph4,
+    {{ normalize_text("VIRUS_ADE") }}           AS result_virus_ade,
+    {{ normalize_text("VIRUS_BOC") }}           AS result_virus_boc,
+    {{ normalize_text("VIRUS_229E") }}          AS result_virus_229e,
+    {{ normalize_text("VIRUS_HKU") }}           AS result_virus_hku,
+    {{ normalize_text("VIRUS_NL63") }}          AS result_virus_nl63,
+    {{ normalize_text("VIRUS_OC43") }}          AS result_virus_oc43,
+    {{ normalize_text("VIRUS_SARS") }}          AS result_virus_sars,
+    {{ normalize_text("VIRUS_COV2") }}          AS result_virus_cov2,
+    {{ normalize_text("VIRUS_EV") }}            AS result_virus_ev,
+    {{ normalize_text("BACTE_BP") }}            AS result_bacte_bp,
+    {{ normalize_text("BACTE_BPAR") }}          AS result_bacte_bpar,
+    {{ normalize_text("BACTE_MP") }}            AS result_bacte_mp,
+
+    file_name
+FROM source_data
---
+++ dbt/models/hlagyn/hlagyn_02_fix_values.sql
@@ -0,0 +1,84 @@
+
+
+{{ config(materialized='table') }}
+{%
+    set state_code_to_state_name = {
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AP': 'Amapá',
+        'AM': 'Amazonas',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MT': 'Mato Grosso',
+        'MS': 'Mato Grosso do Sul',
+        'MG': 'Minas Gerais',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PR': 'Paraná',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RS': 'Rio Grande do Sul',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'SC': 'Santa Catarina',
+        'SP': 'São Paulo',
+        'SE': 'Sergipe',
+        'TO': 'Tocantins'
+    }
+%}
+
+{% set result_column_names = dbt_utils.get_filtered_columns_in_relation(from=ref('hlagyn_01_convert_types'), except=["test_id", "date_testing", "age", "sex", "detalhe_exame", "location", "state_code", "file_name"]) %}
+
+WITH source_data AS (
+    SELECT * 
+    FROM {{ ref('hlagyn_01_convert_types') }}
+)
+SELECT
+    md5(
+        CONCAT(
+            test_id,
+            detalhe_exame
+        )
+    ) AS sample_id,
+    test_id,
+    date_testing,
+    CASE
+        WHEN age > 120 OR age < 0 THEN NULL
+        ELSE age
+    END AS age,
+    CASE
+        WHEN sex ILIKE 'F%' THEN 'F'
+        WHEN sex ILIKE 'M%' THEN 'M'
+        ELSE NULL
+    END AS sex,
+    detalhe_exame,
+    location,
+    state_code,
+    CASE
+        {% for state_code, state_name in state_code_to_state_name.items() %}
+        WHEN state_code = '{{ state_code }}' THEN regexp_replace(upper(unaccent('{{ state_name }}')), '[^\w\s]', '', 'g')
+        {% endfor %}
+        ELSE NULL
+    END AS state,
+    CASE 
+        WHEN result_covid IS NOT NULL THEN 'covid_pcr'
+        WHEN result_virus_influenza_a IS NOT NULL THEN 'test_4'
+        WHEN result_virus_ia IS NOT NULL THEN 'test_24'
+        ELSE 'UNKNOWN'
+    END AS test_kit,
+    {% for column_name in result_column_names %}
+        CASE 
+            WHEN "{{ column_name }}" ILIKE 'NAO DETECTADO%' THEN 0
+            WHEN "{{ column_name }}" ILIKE 'DETECTADO%'     THEN 1
+            WHEN "{{ column_name }}" IS NULL                THEN -1
+            ELSE -2 --UNKNOWN
+        END AS "{{ column_name }}",
+    {% endfor %}
+    file_name
+FROM source_data
