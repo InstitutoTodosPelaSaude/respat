95a8ba2d3a46ee29fcb8926edaac63d24444cf35
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-04-01 17:30:39 -0300

    FEAT: Add einstein sensors

--- dagster/lab_einstein/lab_einstein/definitions.py
+++ dagster/lab_einstein/lab_einstein/definitions.py
@@ -3,14 +3,18 @@ import os
 from dagster import Definitions
 from dagster_dbt import DbtCliResource
 
-from .assets import respiratorios_dbt_assets, einstein_raw
+from .assets import respiratorios_dbt_assets, einstein_raw, einstein_remove_used_files
 from .constants import dbt_project_dir
 from .schedules import schedules
+from .sensors import new_einstein_file_sensor, einstein_slack_failure_sensor
+from .jobs import einstein_all_assets_job
 
 defs = Definitions(
-    assets=[respiratorios_dbt_assets, einstein_raw],
+    assets=[respiratorios_dbt_assets, einstein_raw, einstein_remove_used_files],
     schedules=schedules,
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
     },
+    jobs=[einstein_all_assets_job],
+    sensors=[new_einstein_file_sensor, einstein_slack_failure_sensor],
 )
---
+++ dagster/lab_einstein/lab_einstein/jobs.py
@@ -0,0 +1,3 @@
+from dagster import define_asset_job
+
+einstein_all_assets_job = define_asset_job(name="einstein_all_assets_job")
