8344fbd12e53e9767c03bf2e1cd736e0fc202aad
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-04-16 07:38:04 +0900

    FIX: Convert einstein IDs to BIGINT::TEXT

--- dbt/models/einstein/einstein_01_convert_types.sql
+++ dbt/models/einstein/einstein_01_convert_types.sql
@@ -7,7 +7,7 @@ WITH source_data AS (
 
 )
 SELECT
-    "accession"::BIGINT AS test_id,
+    "accession"::BIGINT::TEXT AS test_id,
     "sexo" AS sex,
     "idade"::INT AS age,
     {{ normalize_text("exame") }} AS exame,
