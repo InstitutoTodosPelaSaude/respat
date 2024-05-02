eff43cef7f4e757e3f3a1804938e66ddf9e47b74
João Pedro <joaopedrodasilvalima@gmail.com>
2024-05-02 18:19:14 -0300

    FEAT: ADENO test result

--- dbt/models/dbmol/dbmol_02_fix_values.sql
+++ dbt/models/dbmol/dbmol_02_fix_values.sql
@@ -17,141 +17,161 @@ source_data_and_method AS (
         --     END 
         -- ) OVER (PARTITION BY test_id, exame) AS method
     FROM source_data
+),
+source_data_fix_values AS (
+    SELECT
+        MD5(
+            CONCAT(
+                test_id,
+                exame,
+                CASE 
+                    WHEN codigo_exame IN ('RESP4') 
+                    THEN codigo_exame
+                    ELSE detalhe_exame
+                END
+            )
+        ) AS sample_id,
+        test_id,
+        date_testing,
+        CASE
+            WHEN EXTRACT( YEAR FROM AGE(date_testing, data_nascimento) )::int > 120
+            THEN NULL
+            ELSE EXTRACT( YEAR FROM AGE(date_testing, data_nascimento) )::int
+        END AS age,
+        CASE 
+            WHEN exame = 'ADENOVIRUS - ANTICORPOS IGG'                            THEN 'adeno_igg'
+            WHEN exame = 'ADENOVIRUS - PESQUISA'                                  THEN 'adeno_test'
+            WHEN exame = 'ADENOVIRUS'                                             THEN 'adeno_test'
+            WHEN exame = 'ANTICORPOS IGA ANTI ADENOVIRUS'                         THEN 'bac_antigen'
+            WHEN exame = 'ANTICORPOS IGA ANTI MYCOPLASMA PNEUMONIAE'              THEN 'bac_antigen'
+            WHEN exame = 'ANTICORPOS IGG ANTI LEGIONELLA PNEUMOPHILA'             THEN 'bac_igg'
+            WHEN exame = 'ANTICORPOS IGG ANTI VIRUS SINCICIAL RESPIRATORIO (VSR)' THEN 'vsr_igg'
+            WHEN exame = 'ANTIGENO DE STREPTOCOCCUS PNEUMONIAE'                   THEN 'bac_antigen'
+            WHEN exame = 'ANTIGENO LEGIONELLA PNEUMOPHILA'                        THEN 'bac_antigen'
+            WHEN exame = 'CORONAVIRUS 2019 - SARS-COV-2 IGG QUANTITATIVO'         THEN 'sc2_igg'
+            WHEN exame = 'DETECCAO DE BORDETELLA PERTUSSIS E PARAPERTUSSIS'       THEN 'bac_test'
+            WHEN exame = 'DETECCAO DE LEGIONELLA PNEUMOPHILA'                     THEN 'bac_test'
+            WHEN exame = 'INFLUENZA A E B - DETECCAO POR PCR'                     THEN 'test_2'
+            WHEN exame = 'INFLUENZA A VIRUS ANTICORPOS IGG'                       THEN 'flua_igg'
+            WHEN exame = 'INFLUENZA A VIRUS ANTICORPOS IGM'                       THEN 'flua_igm'
+            WHEN exame = 'INFLUENZA B VIRUS ANTICORPOS IGG'                       THEN 'flub_igg'
+            WHEN exame = 'INFLUENZA B VIRUS ANTICORPOS IGM'                       THEN 'flub_igm'
+            WHEN exame = 'MYCOPLASMA PNEUMONIAE - ANTICORPOS IGG E IGM'           THEN 'bac_test_2'
+            WHEN exame = 'MYCOPLASMA PNEUMONIAE - ANTICORPOS IGG'                 THEN 'bac_igg'
+            WHEN exame = 'MYCOPLASMA PNEUMONIAE - ANTICORPOS IGM'                 THEN 'bac_igm'
+            WHEN exame = 'PAINEL DE VIRUS RESPIRATORIO SARS-COV-2, VIRUS SINCICIAL, INFLUENZA A, INFLUENZA B' 
+                                                                                THEN 'test_4'
+            WHEN exame = 'PAINEL MOLECULAR PARA DETECCAO DE VIRUS'                THEN 'test_11'
+            WHEN exame = 'PAINEL RESPIRATORIO - PLUS (24 PATOGENOS INCLUINDO SARS COV-2)' 
+                                                                                THEN 'test_24'
+            WHEN exame = 'TESTE DE NEUTRALIZACAO SARS-COV-2/COVID19, ANTICORPOS TOTAIS - SORO' 
+                                                                                THEN 'sc2_antigen'
+            ELSE 'UNKNOWN'
+        END AS test_kit,
+        codigo_exame,
+        detalhe_exame,
+        exame,
+
+        CASE 
+            WHEN codigo_exame IN ('RESP4') 
+            THEN
+                CASE
+                    WHEN result IN ('NAO DETECTADO') THEN 0
+                    WHEN result IN ('DETECTADO') THEN 1
+                    ELSE {{INDETERMINADO}}
+                END 
+
+            -- ADENO (ENZIMAIMUNOENSAIO)
+            WHEN
+                codigo_exame IN ('ADENO')
+            THEN
+                CASE 
+                    WHEN {{ result_is_numeric }} THEN
+                        CASE
+                            WHEN result::FLOAT <= 8.0 THEN 0
+                            WHEN result::FLOAT > 8.0 AND result::FLOAT <= 10.0 THEN {{INDETERMINADO}}
+                            WHEN result::FLOAT > 10.0 THEN 1
+                        END
+
+                    ELSE {{ NAO_RECONHECIDO }}
+                END
+
+            ELSE {{INDETERMINADO}}
+        END AS result,
+
+        CASE sex
+            WHEN 'F' THEN 'F'
+            WHEN 'M' THEN 'M'
+            ELSE NULL
+        END AS sex,
+        location,
+        state_code,
+        file_name
+    FROM source_data_and_method
+    WHERE 1=1
+    AND codigo_exame IN
+    (
+        'MYPNG', 
+        'MYPNM', 
+        'RESP4', 
+        'ADENO', 
+        'INFAM', 
+        'INFAG', 
+        'COVI19Q',
+        'NEUCOV', 
+        'COV19A', 
+        'ADENF', 
+        'MYPNE', 
+        'SINRE', 
+        'INFBG', 
+        'INFBM',
+        'INFLUA',
+        'INFLUB',
+        'HRSVAB',
+        'SARS',
+        'FLUAB', 
+        'BORPC', 
+        'LEGIO', 
+        'LEGPG', 
+        'LEGPN', 
+        'PRESP', 
+        'DNADE',
+        'PVIROC', 
+        'MYPNA', 
+        'ANSP', 
+        'ADENA',
+        'ADENM',
+
+        -- PAINEL RESPIRATÓRIO - PLUS (24 PATÓGENOS INCLUINDO SARS COV-2)
+        'ADENO',
+        'BOCAV',
+        'BPARAP',
+        'BPERTU',
+        'COR0C4',
+        'COR229',
+        'CORHK',
+        'CORNL',
+        'ENTERO',
+        'INFLH3',
+        'INFLU1',
+        'INFLUA',
+        'INFLUB',
+        'METAP',
+        'MYCOP',
+        'PARA1',
+        'PARA2',
+        'PARA3',
+        'PARA4',
+        'RINOV',
+        'SARSC',
+        'SINCIA',
+        'SINCIB'
+    )
+    AND detalhe_exame NOT IN ('MAT', 'MATERIAL', 'METODO', 'SOROTI', 'TITU', 'TIT', 'TITULO', 'LEGPG')
+    AND detalhe_exame IS NOT NULL
+    AND result NOT IN ('INCONCLUSIVO', 'INDETERMINADO')
 )
 SELECT
-    MD5(
-        CONCAT(
-            test_id,
-            exame,
-            CASE 
-                WHEN codigo_exame IN ('RESP4') 
-                THEN codigo_exame
-                ELSE detalhe_exame
-            END
-        )
-    ) AS sample_id,
-    test_id,
-    date_testing,
-    CASE
-        WHEN EXTRACT( YEAR FROM AGE(date_testing, data_nascimento) )::int > 120
-        THEN NULL
-        ELSE EXTRACT( YEAR FROM AGE(date_testing, data_nascimento) )::int
-    END AS age,
-    CASE 
-        WHEN exame = 'ADENOVIRUS - ANTICORPOS IGG'                            THEN 'adeno_igg'
-        WHEN exame = 'ADENOVIRUS - PESQUISA'                                  THEN 'adeno_test'
-        WHEN exame = 'ADENOVIRUS'                                             THEN 'adeno_test'
-        WHEN exame = 'ANTICORPOS IGA ANTI ADENOVIRUS'                         THEN 'bac_antigen'
-        WHEN exame = 'ANTICORPOS IGA ANTI MYCOPLASMA PNEUMONIAE'              THEN 'bac_antigen'
-        WHEN exame = 'ANTICORPOS IGG ANTI LEGIONELLA PNEUMOPHILA'             THEN 'bac_igg'
-        WHEN exame = 'ANTICORPOS IGG ANTI VIRUS SINCICIAL RESPIRATORIO (VSR)' THEN 'vsr_igg'
-        WHEN exame = 'ANTIGENO DE STREPTOCOCCUS PNEUMONIAE'                   THEN 'bac_antigen'
-        WHEN exame = 'ANTIGENO LEGIONELLA PNEUMOPHILA'                        THEN 'bac_antigen'
-        WHEN exame = 'CORONAVIRUS 2019 - SARS-COV-2 IGG QUANTITATIVO'         THEN 'sc2_igg'
-        WHEN exame = 'DETECCAO DE BORDETELLA PERTUSSIS E PARAPERTUSSIS'       THEN 'bac_test'
-        WHEN exame = 'DETECCAO DE LEGIONELLA PNEUMOPHILA'                     THEN 'bac_test'
-        WHEN exame = 'INFLUENZA A E B - DETECCAO POR PCR'                     THEN 'test_2'
-        WHEN exame = 'INFLUENZA A VIRUS ANTICORPOS IGG'                       THEN 'flua_igg'
-        WHEN exame = 'INFLUENZA A VIRUS ANTICORPOS IGM'                       THEN 'flua_igm'
-        WHEN exame = 'INFLUENZA B VIRUS ANTICORPOS IGG'                       THEN 'flub_igg'
-        WHEN exame = 'INFLUENZA B VIRUS ANTICORPOS IGM'                       THEN 'flub_igm'
-        WHEN exame = 'MYCOPLASMA PNEUMONIAE - ANTICORPOS IGG E IGM'           THEN 'bac_test_2'
-        WHEN exame = 'MYCOPLASMA PNEUMONIAE - ANTICORPOS IGG'                 THEN 'bac_igg'
-        WHEN exame = 'MYCOPLASMA PNEUMONIAE - ANTICORPOS IGM'                 THEN 'bac_igm'
-        WHEN exame = 'PAINEL DE VIRUS RESPIRATORIO SARS-COV-2, VIRUS SINCICIAL, INFLUENZA A, INFLUENZA B' 
-                                                                              THEN 'test_4'
-        WHEN exame = 'PAINEL MOLECULAR PARA DETECCAO DE VIRUS'                THEN 'test_11'
-        WHEN exame = 'PAINEL RESPIRATORIO - PLUS (24 PATOGENOS INCLUINDO SARS COV-2)' 
-                                                                              THEN 'test_24'
-        WHEN exame = 'TESTE DE NEUTRALIZACAO SARS-COV-2/COVID19, ANTICORPOS TOTAIS - SORO' 
-                                                                              THEN 'sc2_antigen'
-        ELSE 'UNKNOWN'
-    END AS test_kit,
-    codigo_exame,
-    detalhe_exame,
-    exame,
-
-    CASE 
-        WHEN codigo_exame IN ('RESP4') 
-        THEN
-            CASE
-                WHEN result IN ('NAO DETECTADO') THEN 0
-                WHEN result IN ('DETECTADO') THEN 1
-                ELSE {{INDETERMINADO}}
-            END 
-        ELSE {{INDETERMINADO}}
-
-    END AS result,
-
-    CASE sex
-        WHEN 'F' THEN 'F'
-        WHEN 'M' THEN 'M'
-        ELSE NULL
-    END AS sex,
-    location,
-    state_code,
-    file_name
-FROM source_data_and_method
-WHERE 1=1
-AND codigo_exame IN
-(
-    'MYPNG', 
-    'MYPNM', 
-    'RESP4', 
-    'ADENO', 
-    'INFAM', 
-    'INFAG', 
-    'COVI19Q',
-    'NEUCOV', 
-    'COV19A', 
-    'ADENF', 
-    'MYPNE', 
-    'SINRE', 
-    'INFBG', 
-    'INFBM',
-    'INFLUA',
-    'INFLUB',
-    'HRSVAB',
-    'SARS',
-    'FLUAB', 
-    'BORPC', 
-    'LEGIO', 
-    'LEGPG', 
-    'LEGPN', 
-    'PRESP', 
-    'DNADE',
-    'PVIROC', 
-    'MYPNA', 
-    'ANSP', 
-    'ADENA',
-    'ADENM',
-
-    -- PAINEL RESPIRATÓRIO - PLUS (24 PATÓGENOS INCLUINDO SARS COV-2)
-	'ADENO',
-	'BOCAV',
-	'BPARAP',
-	'BPERTU',
-	'COR0C4',
-	'COR229',
-	'CORHK',
-	'CORNL',
-	'ENTERO',
-	'INFLH3',
-	'INFLU1',
-	'INFLUA',
-	'INFLUB',
-	'METAP',
-	'MYCOP',
-	'PARA1',
-	'PARA2',
-	'PARA3',
-	'PARA4',
-	'RINOV',
-	'SARSC',
-	'SINCIA',
-	'SINCIB'
-
-)
-AND detalhe_exame NOT IN ('MAT', 'MATERIAL', 'METODO', 'SOROTI', 'TITU', 'TIT', 'TITULO', 'LEGPG')
-AND detalhe_exame IS NOT NULL
-AND result NOT IN ('INCONCLUSIVO', 'INDETERMINADO')
+    *
+FROM source_data_fix_values
+WHERE result != {{ INDETERMINADO }}
