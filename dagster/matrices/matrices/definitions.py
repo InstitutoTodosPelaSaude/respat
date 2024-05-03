f60a552df08b89ded2775b3bb390574eac0c2414
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-05-03 21:27:36 +0900

    FEAT: Add sensor for matrices

--- dagster/matrices/matrices/definitions.py
+++ dagster/matrices/matrices/definitions.py
@@ -6,6 +6,8 @@ from dagster_dbt import DbtCliResource
 from .assets import respiratorios_dbt_assets, generate_matrices, adapt_and_rename_matrices, generate_flourish_inputs
 from .constants import dbt_project_dir
 from .schedules import schedules
+from .jobs import matrices_all_assets_job
+from .sensors import run_matrices_sensor, matrices_slack_failure_sensor
 
 defs = Definitions(
     assets=[respiratorios_dbt_assets, generate_matrices, adapt_and_rename_matrices, generate_flourish_inputs],
@@ -13,4 +15,6 @@ defs = Definitions(
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
     },
+    jobs=[matrices_all_assets_job],
+    sensors=[run_matrices_sensor, matrices_slack_failure_sensor],
 )
---
+++ dagster/matrices/matrices/jobs.py
@@ -0,0 +1,3 @@
+from dagster import define_asset_job
+
+matrices_all_assets_job = define_asset_job(name="matrices_all_assets_job")
