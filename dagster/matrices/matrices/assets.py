9dc0068434ae59610c45d4d5bc3eb11c6ca50a7c
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-06-29 23:45:36 +0900

    FEAT: Create matrix 04_SC2_heat_posrate_agegroups_week_country

--- dagster/matrices/matrices/assets.py
+++ dagster/matrices/matrices/assets.py
@@ -52,7 +52,8 @@ def respiratorios_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_NEW_ALL_posrate_pos_neg_by_epiweek"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_NEW_ALL_pos_by_epiweek_agegroup"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_NEW_ALL_posrate_by_epiweek"),
-        get_asset_key_for_model([respiratorios_dbt_assets], "matrix_NEW_FLUA_FLUB_SC2_VSR_pos_by_epiweek")
+        get_asset_key_for_model([respiratorios_dbt_assets], "matrix_NEW_FLUA_FLUB_SC2_VSR_pos_by_epiweek"),
+        get_asset_key_for_model([respiratorios_dbt_assets], "matrix_NEW_SC2_posrate_by_epiweek_agegroup"),
     ]
 )
 def export_matrices_to_xlsx(context):
@@ -64,6 +65,7 @@ def export_matrices_to_xlsx(context):
         "matrix_NEW_ALL_pos_by_epiweek_agegroup": "09_Resp_pyr_pos_agegroups_panel_week_country",
         "matrix_NEW_ALL_posrate_by_epiweek": "01_Resp_line_posrate_panel4_week_country",
         "matrix_NEW_FLUA_FLUB_SC2_VSR_pos_by_epiweek": "02_Resp_bar_pos_panel4_week_country",
+        "matrix_NEW_SC2_posrate_by_epiweek_agegroup": "04_SC2_heat_posrate_agegroups_week_country"
     }
 
     # Get each matrix table and export it to a xlsx file
