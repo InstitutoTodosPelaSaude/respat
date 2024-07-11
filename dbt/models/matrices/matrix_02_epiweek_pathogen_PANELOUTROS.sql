37e537546ae425133c798af995761ac825055bb0
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-07-11 16:46:40 -0300

    FIX: Remove comma

--- dbt/models/matrices/matrix_02_epiweek_pathogen_PANELOUTROS.sql
+++ dbt/models/matrices/matrix_02_epiweek_pathogen_PANELOUTROS.sql
@@ -7,7 +7,7 @@ WITH source_data AS (
         result
     FROM {{ ref("matrices_01_unpivot_combined") }}
     WHERE
-        test_kit IN ('test_14', 'test_21', 'test_24', 'test_4',)
+        test_kit IN ('test_14', 'test_21', 'test_24', 'test_4')
 )
 SELECT
     epiweek_enddate,
