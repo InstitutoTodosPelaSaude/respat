48ab6e550e1bf8ade165c2e8fb4c6d945cca0b13
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-07-15 19:46:25 -0300

    FIX: Remove zeros from matrix

--- dbt/models/matrices/matrix_13_SC2_map_pos_direct_cities.sql
+++ dbt/models/matrices/matrix_13_SC2_map_pos_direct_cities.sql
@@ -16,16 +16,8 @@ WITH source_data AS (
         epiweek_enddate >= '2024-05-19'
     GROUP BY epiweek_enddate, state_code, location, location_ibge_code, lat, long, pathogen
     ORDER BY epiweek_enddate, state_code
-)
-SELECT
-    "semanas epidemiologicas",
-    "location_ibge_code",
-    "location",
-    "state",
-    "lat",
-    "long",
-    SUM("cases") OVER (PARTITION BY "location_ibge_code" ORDER BY "semanas epidemiologicas") as "cases"
-FROM (
+),
+source_data_sum AS (
     SELECT
         epiweek_enddate as "semanas epidemiologicas",
         location_ibge_code as "location_ibge_code",
@@ -37,5 +29,26 @@ FROM (
     FROM source_data
     WHERE location IS NOT NULL
     GROUP BY epiweek_enddate, state_code, location_ibge_code, location, lat, long
+),
+source_data_cumulative_sum AS (
+    SELECT
+        "semanas epidemiologicas",
+        "location_ibge_code",
+        "location",
+        "state",
+        "lat",
+        "long",
+        SUM("cases") OVER (PARTITION BY "location_ibge_code" ORDER BY "semanas epidemiologicas") as "cases"
+    FROM source_data_sum
+    ORDER BY "semanas epidemiologicas", "state", "location"
 )
-ORDER BY "semanas epidemiologicas", "state", "location"
+SELECT
+    "semanas epidemiologicas",
+    "location_ibge_code",
+    "location",
+    "state",
+    "lat",
+    "long",
+    "cases"
+FROM source_data_cumulative_sum
+WHERE "cases" > 0
