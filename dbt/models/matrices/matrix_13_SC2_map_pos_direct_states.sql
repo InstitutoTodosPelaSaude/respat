6785d881e4ca13ef05119ed5b76c02517173f394
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-07-15 18:42:55 -0300

    FIX: Add cummulative sum to the matrix

--- dbt/models/matrices/matrix_13_SC2_map_pos_direct_states.sql
+++ dbt/models/matrices/matrix_13_SC2_map_pos_direct_states.sql
@@ -10,14 +10,19 @@ WITH source_data AS (
     WHERE 
         test_kit NOT IN ('sc2_igg') AND
         epiweek_enddate >= '2024-05-19'
-    GROUP BY epiweek_enddate, state_code, pathogen
-    ORDER BY epiweek_enddate, state_code, pathogen
+    GROUP BY epiweek_enddate, state_code, pathogen    
 )
 SELECT
-    epiweek_enddate as "semanas epidemiologicas",
-    state_code as "state",
-    SUM(CASE WHEN pathogen = 'SC2' THEN "Pos" ELSE 0 END) as "cases"
-FROM source_data
-WHERE state_code IS NOT NULL
-GROUP BY epiweek_enddate, state_code
-ORDER BY epiweek_enddate, state_code
+    "semanas epidemiologicas",
+    "state",
+    SUM("cases") OVER (PARTITION BY "state" ORDER BY "semanas epidemiologicas") as "cases"
+FROM (
+    SELECT
+        epiweek_enddate as "semanas epidemiologicas",
+        state_code as "state",
+        SUM(CASE WHEN pathogen = 'SC2' THEN "Pos" ELSE 0 END) as "cases"
+    FROM source_data
+    WHERE state_code IS NOT NULL
+    GROUP BY epiweek_enddate, state_code
+)
+ORDER BY "semanas epidemiologicas", "state"
