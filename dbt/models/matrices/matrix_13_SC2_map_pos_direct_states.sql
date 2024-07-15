87e4627fa78ced721a9b2c5abe11966ffd6ac780
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-07-15 19:42:30 -0300

    FIX: Remove zeros from matrix

--- dbt/models/matrices/matrix_13_SC2_map_pos_direct_states.sql
+++ dbt/models/matrices/matrix_13_SC2_map_pos_direct_states.sql
@@ -11,12 +11,8 @@ WITH source_data AS (
         test_kit NOT IN ('sc2_igg') AND
         epiweek_enddate >= '2024-05-19'
     GROUP BY epiweek_enddate, state_code, pathogen    
-)
-SELECT
-    "semanas epidemiologicas",
-    "state",
-    SUM("cases") OVER (PARTITION BY "state" ORDER BY "semanas epidemiologicas") as "cases"
-FROM (
+),
+source_data_sum AS (
     SELECT
         epiweek_enddate as "semanas epidemiologicas",
         state_code as "state",
@@ -24,5 +20,18 @@ FROM (
     FROM source_data
     WHERE state_code IS NOT NULL
     GROUP BY epiweek_enddate, state_code
+),
+source_data_cumulative_sum AS (
+    SELECT
+        "semanas epidemiologicas",
+        "state",
+        SUM("cases") OVER (PARTITION BY "state" ORDER BY "semanas epidemiologicas") as "cases"
+    FROM source_data_sum
+    ORDER BY "semanas epidemiologicas", "state"
 )
-ORDER BY "semanas epidemiologicas", "state"
+SELECT
+    "semanas epidemiologicas",
+    "state",
+    "cases"
+FROM source_data_cumulative_sum
+WHERE "cases" > 0
