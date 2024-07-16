6abb59abefd4ae1800c3a08ec9013d02522001d9
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-07-15 21:48:10 -0300

    FIX: Adjust matrix 01

--- dagster/matrices/matrices/assets.py
+++ dagster/matrices/matrices/assets.py
@@ -51,12 +51,12 @@ def respiratorios_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_SC2_posrate_by_epiweek_state_filtered"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_ALL_posrate_pos_neg_by_epiweek"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_ALL_pos_by_epiweek_agegroup"),
-        get_asset_key_for_model([respiratorios_dbt_assets], "matrix_ALL_posrate_by_epiweek"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_FLUA_FLUB_SC2_VSR_pos_by_epiweek_PANEL"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_SC2_posrate_by_epiweek_agegroup"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_FLUA_posrate_by_epiweek_agegroup"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_ALL_posrate_by_epiweek_PANEL"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_ALL_pos_by_epiweek_PANEL"),
+        get_asset_key_for_model([respiratorios_dbt_assets], "matrix_01_VRISP_line_posrate_direct_week_country"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_13_SC2_map_pos_direct_states"),
         get_asset_key_for_model([respiratorios_dbt_assets], "matrix_13_SC2_map_pos_direct_cities"),
     ]
@@ -64,7 +64,7 @@ def respiratorios_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource
 def export_matrices_to_xlsx(context):
     # Map all the db matrix tables that need to be exported to its file name
     matrices_name_map = {
-        "matrix_ALL_posrate_by_epiweek":                    "01_Resp_line_posrate_all_week_country",
+        "matrix_01_VRISP_line_posrate_direct_week_country": "01_VRISP_line_posrate_direct_week_country",
         "matrix_FLUA_FLUB_SC2_VSR_pos_by_epiweek_PANEL":    "02_Resp_bar_pos_panel4_week_country",
         "matrix_SC2_posrate_by_epiweek_state_filtered":     "03_SC2_heat_posrate_all_week_state",
         "matrix_SC2_posrate_by_epiweek_agegroup":           "04_SC2_heat_posrate_all_agegroups_week_country",
---
+++ dbt/models/matrices/matrix_01_VRISP_line_posrate_direct_week_country.sql
@@ -0,0 +1,29 @@
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+    SELECT
+        epiweek_enddate,
+        pathogen,
+        {{ matrices_metrics('result') }}
+    FROM {{ ref("matrices_01_unpivot_combined") }}
+    WHERE
+        CASE
+            WHEN "SC2_test_result" IN ('Neg', 'Pos') THEN test_kit IN ('thermo', 'covid_antigen', 'covid_pcr', 'sc2_antigen', 'test_4', 'test_14', 'test_21', 'test_24')
+            WHEN "FLUA_test_result" IN ('Neg', 'Pos') THEN test_kit IN ('flu_antigen', 'flu_pcr', 'test_3', 'test_4', 'test_14', 'test_2', 'test_21', 'test_24')
+            WHEN "FLUB_test_result" IN ('Neg', 'Pos') THEN test_kit IN ('flu_antigen', 'flu_pcr', 'test_3', 'test_4', 'test_14', 'test_2', 'test_21', 'test_24')
+            WHEN "VSR_test_result" IN ('Neg', 'Pos') THEN test_kit IN ('test_3', 'test_4', 'test_14', 'test_21', 'test_24', 'vsr_antigen')
+            ELSE FALSE
+        END
+    GROUP BY epiweek_enddate, pathogen
+    ORDER BY epiweek_enddate, pathogen
+)
+SELECT
+    epiweek_enddate as "semana epidemiológica",
+    MAX(CASE WHEN pathogen = 'SC2' THEN "posrate" * 100 ELSE NULL END) AS "SARS-CoV-2",
+    MAX(CASE WHEN pathogen = 'FLUA' THEN "posrate" * 100 ELSE NULL END) AS "Influenza A",
+    MAX(CASE WHEN pathogen = 'FLUB' THEN "posrate" * 100 ELSE NULL END) AS "Influenza B",
+    MAX(CASE WHEN pathogen = 'VSR' THEN "posrate" * 100 ELSE NULL END) AS "Vírus Sincicial Respiratório"
+FROM source_data
+GROUP BY epiweek_enddate
+ORDER BY epiweek_enddate
+    
