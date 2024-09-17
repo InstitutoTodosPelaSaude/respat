4bc00c236eb183da62c04e35bea3feeac53958f8
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-09-17 08:55:04 -0300

    FIX: Adjust matrix 04

--- dbt/models/matrices/matrix_04_SC2_heat_posrate_agegroups_week_country.sql
+++ dbt/models/matrices/matrix_04_SC2_heat_posrate_agegroups_week_country.sql
@@ -1,5 +1,7 @@
 {{ config(materialized='table') }}
 
+{% set epiweek_start = '2022-01-01' %}
+
 WITH source_data AS (
     SELECT
         epiweek_enddate,
@@ -7,26 +9,31 @@ WITH source_data AS (
         pathogen,
         {{ matrices_metrics('result') }}
     FROM {{ ref("matrices_01_unpivot_combined") }}
+    WHERE
+        test_kit IN ('covid_antigen', 'covid_pcr', 'sc2_antigen', 'thermo', 'test_4', 'test_14', 'test_21', 'test_23', 'test_24') AND
+        epiweek_enddate >= '{{ epiweek_start }}'
     GROUP BY epiweek_enddate, age_group, pathogen
     ORDER BY epiweek_enddate, age_group, pathogen
 ),
 
 sc2_data AS (
     SELECT
-        'Pos' AS "SC2_test_result",
         age_group AS "faixas etárias",
         epiweek_enddate AS "semana epidemiológica",
-        MAX(CASE WHEN pathogen = 'SC2' THEN "posrate" * 100 ELSE NULL END) AS "percentual"
+        MAX(CASE WHEN pathogen = 'SC2' THEN "posrate" * 100 ELSE NULL END) AS "percentual",
+        SUM(CASE WHEN pathogen = 'SC2' THEN "Pos" ELSE 0 END)::int    AS "pos",
+        SUM(CASE WHEN pathogen = 'SC2' THEN "Neg" ELSE 0 END)::int    AS "neg"
     FROM source_data
     GROUP BY epiweek_enddate, age_group
 )
 
 SELECT
-    "SC2_test_result",
-    "faixas etárias",
-    "semana epidemiológica",
-    "percentual"
+    "semana epidemiológica" AS "Semana epidemiológica",
+    "faixas etárias"        AS "Faixa etária",
+    "percentual"            AS "Taxa de positividade",
+    ("pos" + "neg")::int    AS "Total de testes realizados",
+    "pos"                   AS "Total de positivos",
+    "neg"                   AS "Total de negativos"
 FROM sc2_data
-WHERE "faixas etárias" <> 'NOT REPORTED'
-ORDER BY "semana epidemiológica", "faixas etárias"
+ORDER BY "semana epidemiológica", "faixas etárias" DESC
     
