a392c551807f9c4fd9166b1677f38fd264135db3
João Pedro <joaopedrodasilvalima@gmail.com>
2024-10-29 01:35:18 -0300

    FEAT: HPardini convert types

--- dbt/models/hpardini/hpardini_01_convert_types.sql
+++ dbt/models/hpardini/hpardini_01_convert_types.sql
@@ -3,4 +3,15 @@ WITH source_data AS (
     SELECT * FROM
     {{ source("dagster", "hpardini_raw") }}
 )
-SELECT 1 AS example
+SELECT 
+    "CODIGO" AS test_id,
+    TO_DATE("DATACOLETA", 'DD/MM/YYYY') AS date_testing,
+    {{ normalize_text("PATÓGENO") }} AS pathogen,
+    {{ normalize_text("MÉTODO") }}   AS detalhe_exame,
+    {{ normalize_text("CIDADE") }}   AS location,
+    {{ normalize_text("UF") }}       AS state,
+    "SEXO" AS sex,
+    "IDADE"::BIGINT AS age,
+    {{ normalize_text("RESULTADO") }} AS result,
+    "file_name" 
+FROM source_data
