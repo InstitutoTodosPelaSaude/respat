2959229862e2bb96fb30b8ef334647e73f26b230
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-11-19 11:40:05 -0300

    FEAT: FEAT: Load hilab files from Minio

--- dagster/lab_hilab/lab_hilab/assets.py
+++ dagster/lab_hilab/lab_hilab/assets.py
@@ -14,6 +14,7 @@ from dagster_dbt import (
 from textwrap import dedent
 import pandas as pd
 import os
+import sys
 import pathlib
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
@@ -21,8 +22,10 @@ import shutil
 
 from .constants import dbt_manifest_path
 
-ROOT_PATH = pathlib.Path(__file__).parent.parent.parent.parent.absolute()
-HILAB_FILES_FOLDER = ROOT_PATH / "data" / "hilab"
+sys.path.insert(1, os.getcwd())
+from filesystem.filesystem import FileSystem
+
+HILAB_FILES_FOLDER = "/data/respat/data/hilab/"
 HILAB_FILES_EXTENSION = '.csv'
 
 load_dotenv()
@@ -42,12 +45,14 @@ def hilab_raw(context):
     """
     Table with the raw data from Hilab
     """
+    file_system = FileSystem(root_path=HILAB_FILES_FOLDER)
     engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
 
-    hilab_files = [file for file in os.listdir(HILAB_FILES_FOLDER) if file.endswith(HILAB_FILES_EXTENSION)]
+    hilab_files = [file for file in file_system.list_files_in_relative_path("") if file.endswith(HILAB_FILES_EXTENSION)]
     assert len(hilab_files) > 0, f"No files found in the folder {HILAB_FILES_FOLDER} with extension {HILAB_FILES_EXTENSION}"
 
-    hilab_df = pd.read_csv(HILAB_FILES_FOLDER / hilab_files[0])
+    file_to_get = hilab_files[0].split('/')[-1] # Get the file name
+    hilab_df = pd.read_csv(file_system.get_file_content_as_io_bytes(file_to_get))
     hilab_df['file_name'] = hilab_files[0]
     context.log.info(f"Reading file {hilab_files[0]}")
         
@@ -86,7 +91,8 @@ def hilab_remove_used_files(context):
     Remove the files that were used in the dbt process
     """
     raw_data_table = 'hilab_raw'
-    files_in_folder = [file for file in os.listdir(HILAB_FILES_FOLDER) if file.endswith(HILAB_FILES_EXTENSION)]
+    file_system = FileSystem(root_path=HILAB_FILES_FOLDER)
+    files_in_folder = [file for file in file_system.list_files_in_relative_path("") if file.endswith(HILAB_FILES_EXTENSION)]
 
     # Get the files that were used in the dbt process
     engine = create_engine(f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
@@ -94,13 +100,13 @@ def hilab_remove_used_files(context):
     engine.dispose()
 
     # Remove the files that were used
-    path_to_move = HILAB_FILES_FOLDER / "_out"
+    path_to_move = "_out/"
     for used_file in used_files:
         if used_file in files_in_folder:
             context.log.info(f"Moving file {used_file} to {path_to_move}")
-            shutil.move(HILAB_FILES_FOLDER / used_file, path_to_move / used_file)
+            file_system.move_file_to_folder("", used_file.split("/")[-1], path_to_move)
     
     # Log the unmoved files
-    files_in_folder = os.listdir(HILAB_FILES_FOLDER)
+    files_in_folder = file_system.list_files_in_relative_path("")
     context.log.info(f"Files that were not moved: {files_in_folder}")
 
