04cd58a601adb21d020d10ef35990bc8ea861925
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2024-11-19 13:08:55 -0300

    FEAT: FEAT: Load target files from Minio

--- dagster/lab_target/lab_target/sensors.py
+++ dagster/lab_target/lab_target/sensors.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 from dagster import (
@@ -13,7 +14,10 @@ from dagster.core.storage.dagster_run import FINISHED_STATUSES, DagsterRunStatus
 from dagster_slack import make_slack_on_run_failure_sensor
 
 from .jobs import target_all_assets_job
-from .assets import ROOT_PATH, TARGET_FILES_FOLDER, TARGET_FILES_EXTENSION
+from .assets import TARGET_FILES_FOLDER, TARGET_FILES_EXTENSION
+
+sys.path.insert(1, os.getcwd())
+from filesystem.filesystem import FileSystem
 
 load_dotenv()
 DAGSTER_SLACK_BOT_TOKEN = os.getenv('DAGSTER_SLACK_BOT_TOKEN')
@@ -30,7 +34,8 @@ def new_target_file_sensor(context: SensorEvaluationContext):
     The job will only run if the last run is finished to avoid running multiple times.
     """
     # Check if there are new files in the target folder
-    files = os.listdir(TARGET_FILES_FOLDER)
+    file_system = FileSystem(root_path=TARGET_FILES_FOLDER)
+    files = file_system.list_files_in_relative_path("")
     valid_files = [file for file in files if file.endswith(TARGET_FILES_EXTENSION)]
     if len(valid_files) == 0:
         return
