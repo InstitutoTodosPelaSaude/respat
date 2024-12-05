fa29b9dcea241ab4f0ff63d989ff969f8c81f234
João Pedro <joaopedrodasilvalima@gmail.com>
2024-12-05 01:25:21 -0300

    FEAT: Sivep file sensor

--- dagster/lab_sivep/lab_sivep/definitions.py
+++ dagster/lab_sivep/lab_sivep/definitions.py
@@ -6,6 +6,8 @@ from dagster_dbt import DbtCliResource
 from .assets import respiratorios_dbt_assets, sivep_raw, sivep_remove_used_files
 from .constants import dbt_project_dir
 from .schedules import schedules
+from .sensors import new_sivep_file_sensor, sivep_slack_failure_sensor
+from .jobs import sivep_all_assets_job
 
 defs = Definitions(
     assets=[respiratorios_dbt_assets, sivep_raw, sivep_remove_used_files],
@@ -13,4 +15,6 @@ defs = Definitions(
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
     },
+    jobs=[sivep_all_assets_job],
+    sensors=[new_sivep_file_sensor, sivep_slack_failure_sensor],
 )
---
+++ dagster/lab_sivep/lab_sivep/jobs.py
@@ -0,0 +1,3 @@
+from dagster import define_asset_job
+
+sivep_all_assets_job = define_asset_job(name="sivep_all_assets_job")
