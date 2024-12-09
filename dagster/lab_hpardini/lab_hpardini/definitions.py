97c8472f126972942634442171bece542620d750
João Pedro <joaopedrodasilvalima@gmail.com>
2024-12-09 01:40:07 -0300

    FEAT: Hpardini file sensor

--- dagster/lab_hpardini/lab_hpardini/definitions.py
+++ dagster/lab_hpardini/lab_hpardini/definitions.py
@@ -6,6 +6,8 @@ from dagster_dbt import DbtCliResource
 from .assets import respiratorios_dbt_assets, hpardini_raw
 from .constants import dbt_project_dir
 from .schedules import schedules
+from .jobs import hpardini_all_assets_job
+from .sensors import new_hpardini_file_sensor, hpardini_slack_failure_sensor
 
 defs = Definitions(
     assets=[respiratorios_dbt_assets, hpardini_raw],
@@ -13,4 +15,6 @@ defs = Definitions(
     resources={
         "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
     },
+    jobs=[hpardini_all_assets_job],
+    sensors=[new_hpardini_file_sensor, hpardini_slack_failure_sensor],
 )
