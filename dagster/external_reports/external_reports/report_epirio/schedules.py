e6979183204f6171d4e0bd70acb1ec244eb50a91
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2025-02-03 06:34:20 -0300

    FIX: Change combined and epiweek schedules

--- dagster/external_reports/external_reports/report_epirio/schedules.py
+++ dagster/external_reports/external_reports/report_epirio/schedules.py
@@ -4,6 +4,6 @@ from .jobs import send_report_epirio_email_job
 
 email_report_epirio_schedule = ScheduleDefinition(
     job = send_report_epirio_email_job,
-    cron_schedule = "0 8 * * FRI",
+    cron_schedule = "0 14 * * THU",
     default_status=DefaultScheduleStatus.RUNNING
 )
