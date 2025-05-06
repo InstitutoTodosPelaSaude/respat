3bc732bddac5a687aaa3df090c11589f2bbafa0a
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2025-05-06 17:43:50 -0300

    FIX: Add date_testing to matrices_01

--- dbt/models/matrices/matrices_01_unpivot_combined.sql
+++ dbt/models/matrices/matrices_01_unpivot_combined.sql
@@ -5,6 +5,7 @@ WITH source_data AS (
         sample_id,
         test_kit,
         epiweek_enddate,
+        date_testing,
         epiweek_number,
         lab_id,
         CASE WHEN location IS NULL THEN 'NOT REPORTED' ELSE location END AS location,
