68c24ee9d7e2f3963c7eb6195115d51309410350
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2025-05-07 14:28:32 -0300

    FIX: Add a new value for column Metodologia

--- dagster/lab_hlagyn/lab_hlagyn/assets.py
+++ dagster/lab_hlagyn/lab_hlagyn/assets.py
@@ -65,6 +65,7 @@ def hlagyn_raw(context):
                 'Mátodologia': 'Métodologia',
                 'Metodlogia': 'Métodologia',
                 'metodlogia': 'Métodologia',
+                'Métodologia': 'Métodologia',
             }, inplace=True)
 
     # The columns are not the same for all files, so we need to check the columns
