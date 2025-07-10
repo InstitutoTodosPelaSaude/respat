32a4b5c877d5297f426140d6ca93fc1bed831cd4
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2025-07-10 10:33:27 -0300

    FIX: Change startdate in matrix 17

--- dbt/models/matrices/matrix_17_SC2_line_posrate_bar_pos_direct_week_country_sivep.sql
+++ dbt/models/matrices/matrix_17_SC2_line_posrate_bar_pos_direct_week_country_sivep.sql
@@ -1,5 +1,7 @@
 {{ config(materialized='table') }}
 
+{% set epiweek_start = '2022-03-01' %}
+
 WITH 
 source_data AS (
     SELECT
@@ -9,7 +11,8 @@ source_data AS (
     FROM {{ ref("matrices_01_unpivot_combined") }}
     WHERE
         "SC2_test_result" IN ('Pos', 'Neg') AND
-        test_kit IN ('thermo', 'covid_antigen', 'covid_pcr', 'sc2_antigen', 'test_14', 'test_21', 'test_24', 'test_4')
+        test_kit IN ('thermo', 'covid_antigen', 'covid_pcr', 'sc2_antigen', 'test_14', 'test_21', 'test_24', 'test_4') AND
+        epiweek_enddate >= '{{ epiweek_start }}'
     GROUP BY epiweek_enddate, pathogen
     ORDER BY epiweek_enddate, pathogen
 ),
@@ -21,7 +24,8 @@ sivep_data AS (
         {{ matrices_metrics('result') }}
     FROM {{ ref("matrices_01_unpivot_combined_sivep") }}
     WHERE
-        "SC2_test_result" IN ('Pos', 'Neg')
+        "SC2_test_result" IN ('Pos', 'Neg') AND
+        epiweek_enddate >= '{{ epiweek_start }}'
     GROUP BY epiweek_enddate, pathogen
     ORDER BY epiweek_enddate, pathogen
 ),
