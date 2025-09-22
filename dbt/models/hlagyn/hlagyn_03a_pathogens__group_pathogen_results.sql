d2860db2d50db73539cc0edd97bf4e858d989b72
joaopedro <joaopedrodasilvalima@gmail.com>
2025-09-22 01:17:48 -0300

    FEAT: WIP - STEP 03 - Group results by test id and pathogen

--- dbt/models/hlagyn/hlagyn_03a_pathogens__group_pathogen_results.sql
+++ dbt/models/hlagyn/hlagyn_03a_pathogens__group_pathogen_results.sql
@@ -1,11 +1,37 @@
-
-
 {{ config(materialized='table') }}
+
 
 WITH source_data AS (
     SELECT * 
     FROM {{ ref('hlagyn_02a_pathogens__fix_values') }}
+), results_grouped_by_pathogen AS (
+    SELECT
+        MAX(sample_id) AS sample_id,
+        --test_id,
+        MAX(date_testing) AS date_testing,
+        MAX(age) AS age,
+        MAX(sex) AS sex,
+        MAX(location) AS location,
+        MAX(state_code) AS state_code,
+        MAX(state) AS state,
+
+        MAX(original_test_name) AS original_test_name,
+        MAX(original_test_detail_name) AS original_test_detail_name,
+        
+        MAX(test_kit) AS test_kit,
+        MAX(test_name) AS test_name,
+        BOOL_OR(is_a_multipathogen_test) AS is_a_multipathogen_test,
+        MAX(test_methodology) AS test_methodology,
+
+        --pathogen_name,
+        --pathogen_detail,
+        --pathogen_type,
+        MAX(pathogen_result) AS pathogen_result,
+
+        test_id, pathogen_name, pathogen_type, pathogen_detail
+    FROM source_data
+    GROUP BY test_id, pathogen_name, pathogen_type, pathogen_detail
 )
 SELECT
-*
-FROM source_data
+    *
+FROM results_grouped_by_pathogen
